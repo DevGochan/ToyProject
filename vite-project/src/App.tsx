90a43db5f1bbe2d05a017ce5a6ee72ebd357c87b
Devgochan <devgochan@gmail.com>
2024-12-26 11:40:17 +0900

    firebase init (gitignore -> firebase-config.tsx)

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,10 +2,12 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import TodoPage from './pages/TodoPage';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/TodoPage" element={<TodoPage />} />
--- vite-project/src/components/Header.tsx
+++ vite-project/src/components/Header.tsx
@@ -1,9 +1,52 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
 const Header = () => {
   return (
-    <>
-      <h1>Header</h1>
-    </>
+    <HeaderContainer>
+      <Logo>이것저것 기능구현 사이트</Logo>
+      <Nav>
+        <NavItem>
+          <StyledLink to="/">Home</StyledLink>
+        </NavItem>
+        <NavItem>
+          <StyledLink to="/TodoPage">Todo</StyledLink>
+        </NavItem>
+        <NavItem>
+          <StyledLink to="/Board">게시판-구현중</StyledLink>
+        </NavItem>
+      </Nav>
+    </HeaderContainer>
   );
 };
+
+const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+  background-color: #333;
+  color: white;
+`;
+
+const Logo = styled.h1`
+  font-size: 1.5rem;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+`;
+
+const NavItem = styled.div`
+  margin-left: 2rem;
+`;
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 export default Header;
